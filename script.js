2ae9873227b8965d73c8ccf93c9e14eeb38a824d
Yerzhanov Beksultan <200103513@stu.sdu.edu.kz>
2021-10-19 02:41:18 +0600

    Made navigation bar with some animation and adding first page named Home

---
+++ script.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+    $('.header_burger').click(function(event){
+        $('.header_burger,.header_menu').toggleClass('active');
+        $('body').toggleClass('lock');
+    });
+  
+    
+});
+
+window.addEventListener("scroll", function(){
+    var header = document.querySelector("header");
+    header.classList.toggle("sticky", window.scrollY > 300);
+})
+
